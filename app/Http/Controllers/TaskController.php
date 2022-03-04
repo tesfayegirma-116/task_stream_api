e8fe7d57e7143e59502fde5ce84ef22eb090cb5a
Your Name <tesfayegirma360@gmail.com>
2022-03-04 16:45:55 +0300

    backend task

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tasks;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Tasks::orderBy('created_at', 'desc')->get();
+        return response()->json($tasks);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'tasks' => 'required',
+            'day' => 'required',
+        ]);
+
+        $tasks = new Tasks();
+        $tasks->tasks = $request->tasks;
+        $tasks->day = $request->day;
+        $tasks->reminder = $request->reminder == 1 ? true : false;
+        
+        $tasks->save();
+
+        return response()->json('successfully inserted',$tasks);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tasks $tasks)
+    {
+       
+
+       
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($tasks)
+    {
+        $tasks = Tasks::find($tasks);
+        $tasks->delete();
+        return response()->json(['successfully delated', $tasks]);
+    }
+}
---
+++ database/factories/TasksFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tasks>
+ */
+class TasksFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'tasks' => $this->faker->name(),
+            'day' => $this->faker->Date(),
+        ];
+    }
+}
